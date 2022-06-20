a51e550532c16e894a0cf0a9ac42bf40c2c34d55
philip <ogunye4pao@gmail.com>
2022-06-20 11:16:30 +0100

    Adds tests to update transaction endpoint

--- src/transactions/transactions.service.spec.ts
+++ src/transactions/transactions.service.spec.ts
@@ -33,7 +33,7 @@ describe('TransactionsService', () => {
   });
 
   describe('create', () => {
-    it('It should add a new recurrent payment and return the objects', async () => {
+    it('It should add a new transaction and return the objects', async () => {
       const result = {
         _id: '62b01456b3c98de9c532752b',
         customerID: '62b0119332cd3b1f9cb935bc',
@@ -98,6 +98,62 @@ describe('TransactionsService', () => {
       } catch (error) {
         expect(error.message).toBe('Payment already exists');
       }
+    });
+  });
+
+  describe('updateSingleTransaction', () => {
+    it('It should update a single transaction', async () => {
+      const result = {
+        _id: '62b01456b3c98de9c532752b',
+        customerID: '62b0119332cd3b1f9cb935bc',
+        inputAmount: 1,
+        inputCurrency: 'CEH',
+        outputAmount: 2,
+        outputCurrency: 'PER',
+        createdAt: '2022-06-20T06:31:50.407Z',
+        updatedAt: '2022-06-20T06:31:50.407Z',
+        __v: 0,
+      };
+
+      const ExpectedResult = {
+        _id: '62b01456b3c98de9c532752b',
+        customerID: '62b0119332cd3b1f9cb935bc',
+        inputAmount: 10,
+        inputCurrency: 'CEH',
+        outputAmount: 20,
+        outputCurrency: 'PER',
+        createdAt: '2022-06-20T06:31:50.407Z',
+        updatedAt: '2022-06-20T06:31:50.407Z',
+        __v: 0,
+      };
+
+      const transactionObject = {
+        inputAmount: 10,
+        inputCurrency: 'CEH',
+        outputAmount: 20,
+        outputCurrency: 'PER',
+      };
+
+      jest
+        .spyOn(repo, 'updateOne')
+        .mockImplementation(async () => ExpectedResult as any);
+
+      jest.spyOn(repo, 'findOne').mockImplementation(async () => result as any);
+
+      const resultInfo = await service.updateSingleTransaction(
+        '62b0119332cd3b1f9cb935bc',
+        '62b01456b3c98de9c532752b',
+        transactionObject,
+      );
+
+      expect(resultInfo).toHaveProperty('customerID');
+      expect(resultInfo).toHaveProperty('inputAmount');
+      expect(resultInfo).toHaveProperty('inputCurrency');
+      expect(resultInfo).toHaveProperty('outputAmount');
+      expect(resultInfo).toHaveProperty('outputCurrency');
+      expect(resultInfo).toHaveProperty('_id');
+      expect(resultInfo).toHaveProperty('createdAt');
+      expect(resultInfo).toHaveProperty('updatedAt');
     });
   });
 });
